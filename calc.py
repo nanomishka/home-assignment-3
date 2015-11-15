92de7befff4512899a710bb6d5467fb02b7c2c5f
nanomishka <mihail777str@yandex.ru>
2015-11-15 23:36:10 +0300

    Postfix with inner stack

--- calc.py
+++ calc.py
@@ -1,35 +1,48 @@
 def StrToMathList(str):
     mathList = []
     str += "@"
-    operations = "+-/*"
+    operations = "+-/*()"
     value = ""
 
     for symbol in str:
         if symbol.isdigit():
             value += symbol
         elif symbol in operations:
-            mathList.append(int(value))
+            if value != "":
+                mathList.append(value)
             mathList.append(symbol)
             value = ""
         elif symbol == "@":
-            mathList.append(int(value))
+            mathList.append(value)
 
     return mathList
 
 def MathListToPostfix(mathList):
     ExprData = []
     ExprOp = []
+    prOp = {"+": 0, "-": 0, "*": 1, "/": 1, "(": 0, ")": 1}
 
     for item in mathList:
-        if isinstance(item, int):
+        if item.isdigit():
             ExprData.append(item)
-        else:
-            ExprOp.append(item)
+        else: 
+            if item in "()":
+                if item == ")":
+                    popOp = ExprOp[::-1].index("(")
+                    for pop in range(0, popOp):
+                        ExprData.append(ExprOp.pop())
+                    ExprOp.pop()
+            elif len(ExprOp) > 0 and prOp[item] < prOp[ExprOp[-1]]:
+                popOp = ExprOp[::-1].index("(")
+                for pop in range(0, popOp):
+                    ExprData.append(ExprOp.pop())
+            if item != ")":
+                ExprOp.append(item)
 
     mathList = StrToMathList(mathData)
     return ExprData + ExprOp[::-1]
 
-mathData = "1 + 2 - 3 * 4"
+mathData = "((1+2)*(3+4)*5+6)/7*(6+7)"
 mathList = StrToMathList(mathData)
 postfix = MathListToPostfix(mathList)
 
